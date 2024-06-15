e1d9eff22dd3347aa491014c1b3d201c6dbe971a
Tobex <89360416+Tobexint@users.noreply.github.com>
2024-06-15 07:15:54 +0000

    Update news_scraper.py

--- news_scraper.py
+++ news_scraper.py
@@ -1,5 +1,7 @@
 import openpyxl
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
